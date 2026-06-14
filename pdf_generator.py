612d36ba2a197a0c854492057d91f25f4ae13845
Ram KVS <kalisipudi-venkata.sairam@undp.org>
2026-06-14 23:30:59 +0530

    Remove failed downloads section from generated PDF report

--- pdf_generator.py
+++ pdf_generator.py
@@ -372,22 +372,6 @@ def generate_gender_report_pdf(
 
         elements.append(Spacer(1, 0.4 * inch))
     
-    # Failed Downloads Section (if any)
-    if undownloadable_links:
-        elements.append(PageBreak())
-        elements.append(Paragraph("Failed Downloads", heading_style))
-        elements.append(Spacer(1, 0.2 * inch))
-        
-        failed_text = "The following documents could not be downloaded:<br/><br/>"
-        elements.append(Paragraph(failed_text, body_style))
-        
-        for failed in undownloadable_links:
-            url = failed.get('url', 'Unknown URL')
-            reason = failed.get('reason', 'Unknown reason')
-            
-            failed_item = f'<b>URL:</b> {url}<br/><b>Reason:</b> {reason}<br/><br/>'
-            elements.append(Paragraph(failed_item, body_style))
-    
     # Build PDF
     try:
         doc.build(elements)
